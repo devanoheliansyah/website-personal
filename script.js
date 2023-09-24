2c5b1f84525e811a531be25575bd9e5bef7b5d1c
devanoheliansyah <145905633+devanoheliansyah@users.noreply.github.com>
2023-09-24 18:05:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings : ["adalah seorang pelajar"],
+    typeSpeed: 100,
+    backSpeed: 200,
+    cursorChar: ' | ',
+    smartBackspace: true,
+    loop: TextTrackCue
+});
